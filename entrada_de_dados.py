1c1a25095a41fab71d99379169395ee68b22615f
Abraão Lucas <abraaolucassb@gmail.com>
2023-03-19 00:12:54 -0300

    19-03-2023 (00:12)

---
+++ entrada_de_dados.py
@@ -0,0 +1,28 @@
+"""
+VERSÃO ANTIGA
+
+print("Qual é o seu nome?")
+nome = input()
+
+print("Qual a sua idade?")
+idade = input()
+
+print("Bem-vindo(a) %s que tem %s anos." % (nome, idade))
+"""
+
+"""
+VERSÃO ATUAL
+print("Qual é o seu nome?")
+nome = input()
+
+print("Qual a sua idade?")
+idade = input()
+
+print("Bem vindo {0} que tem a idade de {1} anos!".format(nome, idade))
+"""
+
+# VERSÃO MAIS ATUAL
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual a sua idade? "))
+
+print(f"Bem vindo {nome} que tem a idade de {idade} anos, ou seja, nasceu em {2023 - idade}!")
