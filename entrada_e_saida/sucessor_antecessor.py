91e267543a1c6fd690abe66ea473bb8531f6dd3b
Abraão Lucas <abraaolucassb@gmail.com>
2023-03-25 11:08:16 -0300

    25-03-2023 (11:08)

---
+++ entrada_e_saida/sucessor_antecessor.py
@@ -0,0 +1,9 @@
+"""
+Ler um número inteiro e imprimir seu sucessor e seu antecessor. 
+"""
+
+numero = int(input('Digite um número inteiro: '))
+
+
+print('Antecessor: ', numero - 1)
+print('Sucessor: ', numero + 1)
