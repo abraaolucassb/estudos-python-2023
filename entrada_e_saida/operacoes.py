cc4fbe35bf7a72c69cd98d58ed781930fb00aeb1
Abraão Lucas <abraaolucassb@gmail.com>
2023-03-25 12:07:43 -0300

    25-03-2023 (12:07)

--- entrada_e_saida/operacoes.py
+++ entrada_e_saida/operacoes.py
@@ -7,3 +7,13 @@ Fazer um programa que solicite 2 números e informe:
 -O seno da diferença do primeiro número pelo  segundo;
 -O módulo do primeiro número.
 '''
+
+num1 = float(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo número: '))
+
+soma = num1 + num2
+prod = num1 * num2**2
+quad = num1**2
+raiz = (num1**2) + (num2**2) / 2
+
+print(prod)
