fe3899d1d252ecaa13d9ae43cefcca0c37dac45e
Abraão Lucas <abraaolucassb@gmail.com>
2023-04-07 12:43:15 -0300

    usando for para brincar com variáveis

--- avaliação_modulo_1_xp/atividade1.py
+++ avaliação_modulo_1_xp/atividade1.py
@@ -87,3 +87,24 @@ cpf_sem_pontos = cpf.replace('.', '').replace('-', '')
 
 print(cpf_sem_pontos)
 '''
+
+'''
+5. Crie um código que realize o somatório de todos os caracteres da seguinte
+string:
+numero = '127957'
+Para resolver este problema, considere as seguintes dicas:
+● A soma deverá ser 1 + 2 + 7 + 9 + 5 + 7 = 31;
+● Utilize o laço de repetição for ... in; para percorrer cada caractere da string;
+● Utilize a conversão do tipo
+string para o tipo
+inteiro (int(caractere)) para
+converter os caracteres em valores numéricos;
+● Utilize uma variável auxiliar, soma, para acumular o somatório dos valores.
+'''
+
+numero = '127957'
+soma = 0
+
+for i in numero:
+    soma += int(i)
+print(soma)
